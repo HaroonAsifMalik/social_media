c60788f5e6fc4d2b2c3fd2afeeba40dc551e2656
BiscuitMalik <haroonasifmalik123@gmail.com>
2023-10-12 22:16:57 +0500

    Working.

--- social_media/urls.py
+++ social_media/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 # from users import urls
 
 urlpatterns = [
-#    path("admin/", admin.site.urls),   
+   path("admin/", admin.site.urls),   
    path( '' , include('users.urls') ),
 ]
