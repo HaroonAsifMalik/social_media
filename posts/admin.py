a007fa5f5a2663d8c4c8e59a79a0c83b56db013f
BiscuitMalik <haroonasifmalik123@gmail.com>
2023-10-13 19:33:22 +0500

    Creating Post app models.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Share, Post, Comment, Like
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'image', 'date', 'get_likes', 'get_comments', 'get_shares']
+
+    def get_likes(self, obj):
+        return ", ".join([like.user.username for like in obj.likes.all()])
+    get_likes.short_description = 'Likes'
+
+    def get_comments(self, obj):
+        return ", ".join([comment.comment for comment in obj.comments.all()])
+    get_comments.short_description = 'Comments'
+
+    def get_shares(self, obj):
+        return ", ".join([share.user.username for share in obj.shares.all()])
+    get_shares.short_description = 'Shares'
+
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'share']
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'like']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'comment']
+
+admin.site.register(Share, ShareAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,22 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    like = models.BooleanField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+
+class Share(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    share = models.BooleanField()
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+    date = models.DateField(auto_now=True)
+    likes = models.ManyToManyField(Like)
+    comments = models.ManyToManyField(Comment)
+    shares = models.ManyToManyField(Share)
